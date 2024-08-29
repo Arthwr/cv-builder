de3d3ce13debdd2b28d2150fae863b60564c223d
arthur <artyr007@gmail.com>
2024-08-29 20:32:17 +0500

    chore: add css reset settings and separate components and styles into their own directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import "./App.css";
+import "./assets/styles/App.css";
 
 function App() {
-  return (
-    <>
-      <div>Hello World</div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
